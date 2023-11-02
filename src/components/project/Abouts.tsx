5fd26c5a6d40ac69ebb39540bd0abce284b3333a
David Edem Mensah <davidmensahedem@gmail.com>
2023-11-02 13:57:55 +0000

    Align the contribute section for larger screens

--- src/components/project/Abouts.tsx
+++ src/components/project/Abouts.tsx
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <>
       <div className="bg-h-[100%] bg-w-[100%] bg-project-page-mobile-wave lg:bg-project-page-wave bg-no-repeat bg-cover md:pb-[50px] xl:pb-[80px] xxxl:pb-[80px]">
-        <div className="flex flex-col text-white pt-[125px] md:pt-[200px] lg:pt-[300px] xl:pt-[360px] xxl:pt-[350px] xxxl:pt-[500px] xxxxl:pt-[600px]">
-          <h1 className="text-center mt-[25px] sm:mt-[50px] md:mt-[40px] lg:mt-[60px] xl:mt-[20px] xxl:mt-[100px] xxxl:mt-[93.38px] text-[16px] uppercase md:text-[40px] font-[600] leading-[23.38px] md:leading-[48px]">
+        <div className="flex flex-col text-white pt-[125px] md:pt-[200px] lg:pt-[300px] xl:pt-[360px] xxl:pt-[350px] xxxl:pt-[400px] xxxxl:pt-[600px]">
+          <h1 className="text-center mt-[25px] sm:mt-[50px] md:mt-[40px] lg:mt-[60px] xl:mt-[20px] xxl:mt-[100px] xxxl:mt-[93.38px] text-[16px] uppercase md:text-[40px] xxxl:text-[64px] font-[600] leading-[23.38px] md:leading-[48px]">
             About the Project
           </h1>
-          <div className="mb-[80px] md:mb-[15px] lg:mb-[20px] mt-[20px] lg:mt-[50px] xl:mt-[30px] px-[39px] sm:px-[39px] md:px-[150px] lg:px-[210px] xl:px-[380px] xxxl:px-[700px] xxxxl:px-[800px]">
+          <div className="mb-[80px] md:mb-[15px] lg:mb-[20px] mt-[20px] lg:mt-[50px] xxxl:mt-[60px] xl:mt-[30px] px-[39px] sm:px-[39px] md:px-[150px] lg:px-[210px] xl:px-[380px] xxxl:px-[400px] xxxxl:px-[700px]">
             <p className="text-center text-[15px] md:text-[16px] leading-[25px] sm:leading-[30px] md:leading-[32px] font-[400]">
               The GPGP project represents a collaborative endeavour between
               InfinArt and the esteemed environmental artist John Dahlsen. It
--- src/components/project/ProjectHero.tsx
+++ src/components/project/ProjectHero.tsx
@@ -3,24 +3,23 @@ import gpgpMapImage from "../../assets/images/gpgp-map-mobile.png";
 const ProjectHero = () => {
   return (
     <>
-      <div className="sm:flex w-[100%] mb-[-20px] md:mb-[-10px] lg:mb-[-100px] xl:mb-[-80px] xxl:mb-[-80px] xxxl:mb-[-220px]">
+      <div className="sm:flex w-[100%] mb-[-20px] md:mb-[-10px] lg:mb-[-100px] xl:mb-[-80px] xxl:mb-[-80px] xxxl:mb-[-70px] xxxxl:mb-[-220px]">
         <div className="h-[100%] w-[100%] z-[4px] opacity-[1] md:z-[400] z-[400]">
           <div className="flex flex-col w-[100%] items-center justify-center mx-auto uppercase font-[700] md:leading-[59px] md:text-[40px] lg:gap-y-[30px]">
             <div className="mt-[44.21px] mb-[31.96px]">
-              <h1 className="leading-[29.5px] text-[20px] md:leading-[29.5px] font-bold md:font-[600px] md:text-[30px] lg:text-[40px] text-center lg:leading-[59px] md:leading-[55px]">
+              <h1 className="leading-[29.5px] text-[20px] md:leading-[29.5px] font-bold md:font-[600px] md:text-[30px] lg:text-[40px] xxxxl:text-[64px] text-center lg:leading-[59px] xxxxl:leading-[94.41px] md:leading-[55px]">
                 The Great Pacific Garbage Patch Art Project{" "}
               </h1>
             </div>
             <img
               src={gpgpMapImage}
               alt={"GPGP Map"}
-              className="w-[300px] h-[185px] mb-[20px] mx-auto md:w-[680px] md:h-[493px] lg:w-[804px] lg:h-[493px]"
+              className="w-[300px] h-[185px] mb-[20px] mx-auto md:w-[680px] md:h-[493px] lg:w-[804px] lg:h-[493px] xxxl:w-[1243px] xxxl:h-[762px]"
             />
             <div className="">
-              <h6 className="font-[500] text-[16px] md:text-[32px] text-center">
+              <h6 className="font-[500] text-[16px] md:text-[32px] xxxxl:text-[64px] text-center xxxxl:leading-[94.41px]">
                 contribute to the global effort
-              </h6>
-              <h6 className="font-[500] text-[16px] md:text-[32px] text-center">
+                <br />
                 Create a better world
               </h6>
             </div>
