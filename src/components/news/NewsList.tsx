6207b60a70cb84707ca42be6db8a128b72de9500
annassiddique <annassiddique7500@gmail.com>
2023-12-05 18:31:37 +0500

    fixing-bugs-on-newspage

--- src/components/news/NewsList.tsx
+++ src/components/news/NewsList.tsx
@@ -9,6 +9,7 @@ import prismicDocumentTypes from "../../utility/prismicDocumentTypes";
 const NewsList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [currentCategory, setCurrentCategory] = useState("All");
+  const [showAllCategories, setShowAllCategories] = useState(true);
 
   const [document] = useSinglePrismicDocument(prismicDocumentTypes.news);
   // console.log(document);
@@ -17,50 +18,61 @@ const NewsList = () => {
     {
       title: "GPGP news",
       description: "The Tide of Plastic A Growing Threat",
-      primary :document?.data["body"][1].primary,
-      prismicItems : document?.data["body"][1].items,
+      primary: document?.data["body"][1].primary,
+      prismicItems: document?.data["body"][1].items,
     },
     {
       title: "Plastic pollution",
       description: "The Tide of Plastic A Growing Threat",
-      primary :document?.data["body"][2].primary,
-      prismicItems : document?.data["body"][2].items,
+      primary: document?.data["body"][2].primary,
+      prismicItems: document?.data["body"][2].items,
     },
     {
       title: "ENVIRonmental art",
       description: "The Tide of Plastic A Growing Threat",
-      primary :document?.data["body"][3].primary,
-      prismicItems : document?.data["body"][3].items,
+      primary: document?.data["body"][3].primary,
+      prismicItems: document?.data["body"][3].items,
     },
     {
       title: "Events",
       description: "The Tide of Plastic A Growing Threat",
-      primary :document?.data["body"][4].primary,
-      prismicItems : document?.data["body"][4].items,
+      primary: document?.data["body"][4].primary,
+      prismicItems: document?.data["body"][4].items,
     },
     {
       title: "Test",
       description: "The Tide of Plastic A Growing Threat",
-      primary :document?.data["body"][5].primary,
-      prismicItems : document?.data["body"][5].items,
+      primary: document?.data["body"][5].primary,
+      prismicItems: document?.data["body"][5].items,
     },
   ];
 
-
   const itemsPerPage = 1;
-  let items = categories ?? [];
 
   // Get the category of items
   const tabCategories: string[] = [];
-  categories.map((item) => tabCategories.push(item.title.toUpperCase()));
+  categories.forEach((item) => tabCategories.push(item.title.toUpperCase()));
 
   const handleCategoryChange = (category: string) => {
     setCurrentCategory(category);
     setCurrentPage(1);
+    setShowAllCategories(category === "All");
   };
 
+  // Filter items based on the current category
+  const filteredItems = showAllCategories
+    ? categories
+    : categories.filter(
+        (category) =>
+          category.title.toUpperCase() === currentCategory.toUpperCase()
+      );
+
   // Pagination
-  const totalItems = items.length;
+  const totalItems = filteredItems.length;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = showAllCategories ? totalItems : startIndex + itemsPerPage;
+  const displayedItems = filteredItems.slice(startIndex, endIndex);
+
   const handlePageChange = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
@@ -71,12 +83,12 @@ const NewsList = () => {
         currentCategory={currentCategory}
         onCategoryChange={handleCategoryChange}
       />
-      {items.map((category, index) => (
+      {displayedItems.map((category, index) => (
         <NewsCategory
           key={index}
           category={category}
-          primary={categories[index].primary}
-          prismicItems={categories[index].prismicItems}
+          primary={category.primary}
+          prismicItems={category.prismicItems}
         />
       ))}
 
