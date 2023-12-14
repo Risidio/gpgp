ee5e86ffd83893be766ef0a3a6f556eed96c4f61
David Edem Mensah <davidmensahedem@gmail.com>
2023-12-14 13:24:07 +0000

    Correct the terinary if check

--- src/components/contribute/Invitation/InviteHero.tsx
+++ src/components/contribute/Invitation/InviteHero.tsx
@@ -11,7 +11,7 @@ export default function InviteHero({
       <div className="w-full flex flex-col justify-center text-center items-center text-gpgp-blue pt-12 ">
         <h3 className="text-[1rem] uppercase font-bold xxsm:text-[1rem]  md:text-[1.4rem]  xl:text-[1.5rem]  xxxxl:text-[1.9rem]  
        tracking-[0.3rem]  sm:tracking-[0.4rem]  xxl:tracking-[0.4rem] xxxxl:tracking-[0.5rem]
-        ">You’re an {heading ===  "Sponsor"? "Investor": heading}</h3>
+        ">You’re an {heading ===  "Sponsor" ? "Investor": heading}</h3>
         <h1 className="text-[1.2rem] mt-[9px] sm:mt-[0px] font-semibold xxsm:text-[1.5rem]  md:text-[3rem]  xl:text-[3.6rem]  xxxxl:text-[4rem]">
           And you’d like to <span className="text-[#D9BF4F] underline"> Contribute</span>
         </h1>
