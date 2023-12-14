f5db094ec0c6c4c0f7d85bbfcdc6073a455d00ee
David Edem Mensah <davidmensahedem@gmail.com>
2023-12-14 09:36:53 +0000

    Challenge page re-design

--- src/components/challenge/Section3.tsx
+++ src/components/challenge/Section3.tsx
@@ -1,51 +1,46 @@
-import FlatLayComposition from "../../assets/images/flat-lay-composition-different-colored-plastic-bags.png";
+import FlatLayComposition from "../../assets/images/flat-lay-composition-different-colored-plastic-bags-mobile.jpg";
 
 const Section3 = () => {
   return (
     <>
-      <div className="">
-        <div className="flex flex-col items-center justify-center"></div>
-        <div className="grid md:grid-cols-2">
-          <div className="order-2 md:order-1 flex flex-col justify-center">
-            <div className="hidden md:block md:ml-[64px] md:mt-[80px]">
-              <h1 className="text-[#050B1D] font-[be-vietnam-bold] md:text-4xl font-bold leading-[147.51%]">
-                Heading in the right direction
-              </h1>
-            </div>
-            <div className="flex flex-col items-center gap-y-[20px] mt-[30px] md:mt-[35px] mx-[23px] justify-center md:ml-[64px] md:mr-[87px] md:pb-[40px]">
-              <p className="text-center md:text-left text-[#050B1D] font-[be-vietnam] text-[16px] xl:text-[20px] xxxl:text-[25px] leading-[200%] md:leading-[22px] lg:leading-[35px] xl:leading-[40px] sm:leading-[30px] md:leading-[32px] font-[400]">
-                The progress achieved in addressing the issue of plastic
-                pollution demonstrates a positive trajectory toward
-                environmental sustainability and conservation efforts.
-              </p>
-              <p className="text-center md:text-left text-[#050B1D] font-[be-vietnam] text-[16px] xl:text-[20px] xxxl:text-[25px] leading-[200%] md:leading-[22px] lg:leading-[35px] xl:leading-[40px] sm:leading-[30px] md:leading-[32px] font-[400]">
-                <b>The Project GPGP will inspire researchers and engineers to
-                develop innovative solutions</b> to efficiently remove plastic from
-                oceans, implementing policies to reduce plastic usage which
-                enables local communities and organizations to actively
-                participate in raising awareness, and educating people about the
-                importance of adopting sustainable choices.
-              </p>
-            </div>
-          </div>
-          <div className="w-full md:h-full order-1 md:order-2 bg-[length:100%_100%] md:bg-none md:border-y-[0px] border-gpgp-blue">
-          <div className="md:hidden md:w-0 w-full mt-[100px] mb-[30px]">
-              <div className="md:hidden w-full h-full flex flex-col justify-center items center">
-                <h1 className="text-center text-black text-[16px] uppercase font-bold leading-[32px]">
-                  Heading in the right direction
-                </h1>
-              </div>
-            </div>
-
+      <section className="mt-[100px] xl:mt-[368px] xl:grid xl:grid-cols-2 xl:gap-x-[100px] xxl:gap-x-[125.25px] xl:px-[50px] xxl:px-[80px]">
+        <div className="xl:order-2">
+          <h3 className="xl:hidden w-[257px] text-[#050B1D] text-center font-[be-vietnam-700] uppercase mx-auto text-[16px]">
+            Heading in the right direction
+          </h3>
+          {/* Image */}
+          <div className="mt-[39px] xl:mt-0 flex flex-col items-center xl:items-start">
             <img
               src={FlatLayComposition}
-              className="md:block w-full h-[196px] md:h-[100%] md:w-[740px] lg:w-full"
-              alt="Flat Lay Composition Different Colored"
+              alt="Trashed Plastic Objects"
+              className="w-[293px] h-[199px] xl:w-[630px] xl:rounded-[20px] xl:h-[630px]"
             />
-            
           </div>
         </div>
-      </div>
+
+        {/* Contents */}
+        <div className="mt-[36px] xl:mt-0 w-[293px] xl:w-[556px] text-center xl:text-left text-[#050B1D] mx-auto xl:mx-0 font-[be-vietnam] bold-400 xl:flex xl:items-center">
+          <div>
+            <h3 className="hidden xl:block text-[36px] leading-[53.104px] text-[#050B1D] font-[be-vietnam-700]">
+              Heading in the right direction
+            </h3>
+
+            <p className="tracking-[0.5px] text-[15px] xl:text-[16px] leading-[30px] xl:leading-[32px] xl:tracking-[-0.32px] xl:mt-[62px] xl:w-[523px]">
+              The progress achieved in addressing the issue of plastic pollution
+              demonstrates a positive trajectory toward environmental
+              sustainability and conservation efforts.
+            </p>
+            <p className="mt-[25px] text-[15px] xl:text-[16px] leading-[30px] xl:leading-[32px] tracking-[0.2px] xl:tracking-[-0.32px] xl:w-[523px]">
+              The Project GPGP will inspire researchers and engineers to develop
+              innovative solutions to efficiently remove plastic from oceans,
+              implementing policies to reduce plastic usage which enables local
+              communities and organizations to actively participate in raising
+              awareness, and educating people about the importance of adopting
+              sustainable choices.
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/components/gallery/GalleryHeader.tsx
+++ src/components/gallery/GalleryHeader.tsx
@@ -5,20 +5,20 @@ const GalleryHeader = () => {
 
   const handleSetActiveTab = (tab:string) => setActiveTab(tab);
 
-  const tabs = [
-    {
-      title: "All",
-    },
-    {
-      title: "Early Work",
-    },
-    {
-      title: "Environmental Work",
-    },
-    {
-      title: "Invited Artist Work",
-    },
-  ];
+  // const tabs = [
+  //   {
+  //     title: "All",
+  //   },
+  //   {
+  //     title: "Early Work",
+  //   },
+  //   {
+  //     title: "Environmental Work",
+  //   },
+  //   {
+  //     title: "Invited Artist Work",
+  //   },
+  // ];
 
   return (
     <>
@@ -35,7 +35,7 @@ const GalleryHeader = () => {
               </h3>
             </div>
 
-            <div>
+            {/* <div>
               <div className="flex gap-x-[5px] lg:gap-x-[20px]">
                 {tabs.map((tab) => (
                   <span
@@ -47,7 +47,7 @@ const GalleryHeader = () => {
                   </span>
                 ))}
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
